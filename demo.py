96d0862ef766a4852f5a99e2e7aca5cae8fdc88e
Pranav Sharma <110465335+PRANAV7389@users.noreply.github.com>
2023-03-31 16:07:42 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price app
+## (Rakh lena na job mai apne company mai pleaseeeeeeee)
+
+Shown are the stock closing price and volume of google!
+""")
+
+tickerSymbol = 'Googl'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+tickerDf = tickerData.history(period = 'id',start = '2010-5-31', end = '2020-5-31')
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
